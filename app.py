151d23f6dfe551967f4360ede815d7173e02488a
dakearts <sergy.chelsea@gmail.com>
2025-06-16 20:09:28 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,8 +2,25 @@ from PIL import Image
 
 image = Image.open("lenna.jpg")
 rgb_image = image.convert("RGB")
-red, green, blue = rgb_image.split()
-red.save("red_lenna.jpg")
-green.save("green_lenna.jpg")
-blue.save("blue_lenna.jpg")
-print(f"Ширина - {rgb_image.width}\nВысота - {rgb_image.height}\nЦветовая модель - {rgb_image.mode}")
+red_channel, green_channel, blue_channel = rgb_image.split()
+image = Image.merge("RGB", (red_channel, green_channel, blue_channel))
+
+offset = 50
+
+right_shift_coordinates = (0, 0, image.width-offset, image.height)
+left_shift_coordinates = (offset, 0, image.width, image.height)
+middle_shift_coordinates = (offset/2, 0, image.width-offset/2, image.height)
+
+middle_red = red_channel.crop(right_shift_coordinates)
+middle_green = green_channel.crop(left_shift_coordinates)
+middle_blue = blue_channel.crop(middle_shift_coordinates)
+
+cropped_red = red_channel.crop(left_shift_coordinates)
+cropped_blue = blue_channel.crop(right_shift_coordinates)
+
+blended_red = Image.blend(cropped_red, middle_red, 0.5)
+blended_blue = Image.blend(cropped_blue, middle_blue, 0.5)
+
+blended_image = Image.merge("RGB", (blended_red, middle_green, blended_blue))
+blended_image.thumbnail((80, 80))
+blended_image.save("blended_image.jpg")
